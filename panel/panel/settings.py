b2ab5b7860a0e4adf7db6547fd0c042d96c4d3d1
Fan Wang <nimadorostkar97@gmail.com>
2024-09-18 15:57:26 +0330

    push

--- panel/panel/settings.py
+++ panel/panel/settings.py
@@ -106,6 +106,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "/static/"
+MEDIA_ROOT =BASE_DIR/"media"
+MEDIA_URL = "/media/"
+STATICFILES_DIRS = ["docs/"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- panel/panel/urls.py
+++ panel/panel/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from link.views import LinkView
+from django.conf.urls.static import static
 
+from panel.settings import STATIC_ROOT, STATIC_URL, MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("links", LinkView.as_view(), name="links"),
 ]
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
+urlpatterns += static(STATIC_URL, document_root=STATIC_ROOT)
