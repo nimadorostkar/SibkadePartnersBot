60164659c61d6b61469bbb43d97182559c44efe8
Fan Wang <nimadorostkar97@gmail.com>
2024-10-28 11:55:41 +0330

    AddOrderView

--- panel/order/admin.py
+++ panel/order/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from order.models import Order
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('order_code', 'user', 'expiration', 'create_at')
-    list_filter = ("user", "expiration")
+    list_display = ('order_code', 'status', 'user', 'expiration', 'create_at')
+    list_filter = ("user", "expiration", "status")
     search_fields = ['order_code', 'user']
 admin.site.register(Order, OrderAdmin)
